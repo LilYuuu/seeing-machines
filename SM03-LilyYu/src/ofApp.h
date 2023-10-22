3eba1f1f088bc54329f669745b51ba3d0926e497
Lily Yu <>
2023-10-22 17:10:27 -0400

    week3 uploaded

---
+++ SM03-LilyYu/src/ofApp.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "ofMain.h"
+//#include "ofxCv.h"
+#include "ofxOpenCv.h"
+#include "ofxGui.h"
+#include "ofxBox2d.h"
+#include "ofxKinect.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup() override;
+		void update() override;
+		void draw() override;
+        
+        void createContourEdge(ofPolyline polyline);
+    
+        ofxKinect kinect;
+    
+        ofxButton uiLearn;
+        ofxButton uiClearCircles;
+        ofxButton uiClearContourEdge;
+        ofxIntSlider uiThreshold;
+    
+        ofxCvColorImage color;
+        ofxCvGrayscaleImage grayscale;
+        ofxCvGrayscaleImage background;
+        ofxCvGrayscaleImage difference;
+        ofxCvContourFinder contour;
+
+        ofxBox2d box2d;
+        vector <shared_ptr<ofxBox2dCircle>> circles;
+        ofxBox2dEdge *contourEdge;
+    
+    
+//        ofImage thresholdImg;
+//
+//        ofParameter<float> nearThreshold;
+//        ofParameter<float> farThreshold;
+
+    
+        ofxPanel gui;
+    
+		void exit() override;
+
+		void keyPressed(int key) override;
+//		void keyReleased(int key) override;
+//		void mouseMoved(int x, int y ) override;
+//		void mouseDragged(int x, int y, int button) override;
+		void mousePressed(int x, int y, int button) override;
+//		void mouseReleased(int x, int y, int button) override;
+//		void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
+//		void mouseEntered(int x, int y) override;
+//		void mouseExited(int x, int y) override;
+//		void windowResized(int w, int h) override;
+//		void dragEvent(ofDragInfo dragInfo) override;
+//		void gotMessage(ofMessage msg) override;
+		
+};
